d463ee4e87ea90c9e69e7ec28aeb2208627ac9f9
Irene <hnin.yeminymster@gmail.com>
2020-01-17 05:27:19 +0630

    .

--- src/Customquery.js
+++ src/Customquery.js
@@ -1,9 +1,31 @@
 import  React, {Fragment} from "react";
 import  {gql} from "apollo-boost";
-import { Query} from "react-apollo";
+import { Query, Mutation} from "react-apollo";
 
-const COUNT_DAY_QUERY = gql`query countDays{totalDays}`;
+const COUNT_DAY_QUERY = gql`
+    query countDays{
+        totalDays
+    }
+`;
 
+const ADD_DAY_MUTATION = gql`
+    mutation add {
+        addDay
+    }
+`
+const REMOVE_DAY_MUTATION = gql`
+    mutation remove {
+        removeDay
+    }
+`;
+
+const updateLocalCount = (cache, {data}) => {
+    const totalDays = data.addDay || data.removeDay;
+    cache.writeQuery({
+        query: COUNT_DAY_QUERY,
+        data: {totalDays}
+    })
+}
 const Customquery =() =>(
     <Fragment>
         <h1> SKi Days</h1>
@@ -15,6 +37,17 @@ const Customquery =() =>(
             return <p> total days: {data.totalDays}</p>
         }}
     </Query>
+    <Mutation mutation={ADD_DAY_MUTATION}
+        update={updateLocalCount}
+    >
+        {addDay => <button onClick={addDay}>+</button>}
+    </Mutation>
+    <Mutation mutation={REMOVE_DAY_MUTATION}
+        update={updateLocalCount}
+    >
+        {removeDay => <button onClick={removeDay}>-</button>}
+    </Mutation>
+    
     </Fragment>
 );
 
